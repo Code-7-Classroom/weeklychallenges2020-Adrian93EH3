33700bf8ace6b00b1e353b68629c99d93d2e6d51
Adrian Daniel Diaz <Adrian@MacBook-Pro-2.lan1>
2020-03-30 23:18:04 -0400

    Javascript Week 2 Challenges

---
+++ JavaScript-Week-2-Challenges/easy.js
@@ -0,0 +1,5 @@
+var lastNames = [ 'Doe', 'Diaz', 'Doe'];
+var firstNames = ['John', 'Adrian', 'Jane'];
+var ages = [22, 21, 23];
+
+console.log("Hello my name is " + firstNames[1] + ' ' + lastNames[1] + " and I am " + ages[0] + " years old!" );
